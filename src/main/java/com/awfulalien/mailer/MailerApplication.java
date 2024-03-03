47e0a8e84e2edf807396383966fb9235ac643696
AwfulAlien <dhana22busy@gmail.com>
2024-03-03 22:12:34 +0530

    simple mailer functionality implemented

--- src/main/java/com/awfulalien/mailer/MailerApplication.java
+++ src/main/java/com/awfulalien/mailer/MailerApplication.java
@@ -1,13 +1,30 @@
 package com.awfulalien.mailer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MailerApplication {
 
+    @Autowired
+    private MailerService mailerService;
+
     public static void main(String[] args) {
         SpringApplication.run(MailerApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() {
+
+        // provide values hardcoded or with Scanner
+        String to = "testmail@gmail.com";
+        String subject = "test subject";
+        String body = "test body";
+
+        mailerService.sendMail(to, subject, body);
     }
 
 }
--- src/main/java/com/awfulalien/mailer/MailerService.java
+++ src/main/java/com/awfulalien/mailer/MailerService.java
@@ -1,6 +1,7 @@
 package com.awfulalien.mailer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,20 @@ public class MailerService {
     private JavaMailSender mailSender;
 
     void sendMail(String to, String subject, String body) {
+
+        //mail id which you have provided in the application.properties file
+        String fromMailID = "";
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(fromMailID);
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(body);
+
+        mailSender.send(mailMessage);
+
+        System.out.println("E-Mail with the Subject :" + " '" + subject + "' " + "has been sent to " + to );
 
     }
 
